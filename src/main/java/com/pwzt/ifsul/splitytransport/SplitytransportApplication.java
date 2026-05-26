ea4da7bd80584952e301c4fb9fb57f1ed04c9561
allan-thro <thro.allan@gmail.com>
2026-05-26 20:52:38 -0300

    feat: first commit

---
+++ src/main/java/com/pwzt/ifsul/splitytransport/SplitytransportApplication.java
@@ -0,0 +1,13 @@
+package com.pwzt.ifsul.splitytransport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitytransportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitytransportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pwzt/ifsul/splitytransport/SplitytransportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pwzt.ifsul.splitytransport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitytransportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
